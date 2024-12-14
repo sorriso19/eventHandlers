0e0979c7bc2735f1eed4e967ba0e77a9450e9f20
Irina Zelenova <sorriso19@mail.ru>
2024-12-14 21:50:05 +0300

    убрала text.focus(), всё работает

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const changeColor = function (event) {
    
     square.style.backgroundColor = text.value;
     text.value = '';
-    text.focus();
+    
 };
 
 button.addEventListener('click', changeColor)
