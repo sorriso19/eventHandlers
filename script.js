4f4db97591f630d3bc0a927e646700d4ae1f6918
Irina Zelenova <sorriso19@mail.ru>
2024-12-14 21:30:53 +0300

    исправлены методы и функции

--- script.js
+++ script.js
@@ -3,29 +3,28 @@
 const button = document.getElementById('btn')
 const text = document.getElementById('text')
 const range = document.getElementById('range')
-const square = document.querySelector('square')
+const square = document.querySelector('#square')
 const eButton = document.getElementById('e_btn')
-const circle = document.getElementById('circle')
+const rangeSpan = document.getElementById('range-span')
 
-
-const changeColor = function (event) { 
-    let input = this.elements;
+const changeColor = function (event) {
+   
     square.style.backgroundColor = text.value;
     text.value = '';
     text.focus();
-}
- 
+};
+
 button.addEventListener('click', changeColor)
 
 const buttonStyle = function () {
     eButton.style = "display:none;"
-}
+};
 
-const rangeChange = function(event) {
-    
-    circle.textContent = event.target.value
-}
+eButton.addEventListener('click', buttonStyle)
+
+const rangeChange = function (event) {
+
+    rangeSpan.textContent = event.target.value
+};
 
 range.addEventListener('input', rangeChange)
-
-
